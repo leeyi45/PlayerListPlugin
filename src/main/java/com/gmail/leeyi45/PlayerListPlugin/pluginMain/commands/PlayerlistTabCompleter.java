4cb4d0c5c353cbceb9ce6a3aee128f052834d8da
leeyi45 <leeyi45@gmail.com>
2019-10-04 13:34:03 +0800

    Added Tab Completion to command

---
+++ src/main/java/com/gmail/leeyi45/PlayerListPlugin/pluginMain/commands/PlayerlistTabCompleter.java
@@ -0,0 +1,28 @@
+package com.gmail.leeyi45.PlayerListPlugin.pluginMain.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PlayerlistTabCompleter implements TabCompleter
+{
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command,
+                                                String s, String[] args)
+    {
+        switch(args.length)
+        {
+            case 0: return Arrays.asList("discord", "telegram", "ip");
+            case 1: return Arrays.asList("status", "disconnect", "reconnect", "token");
+            case 2:
+            {
+                if(args[1].equalsIgnoreCase("token")) return Arrays.asList("set", "get");
+                else return null;
+            }
+            default: return null;
+        }
+    }
+}
